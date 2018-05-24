fc31b4809f37aeff031b85c85f5410f284aa4b20
Denys Siroshtan <denis.siroshtan@mgid.com>
2018-05-24 18:32:35 +0300

    update

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,8 +1,8 @@
 // Import and configure the Firebase SDK
 // These scripts are made available when the app is served or deployed on Firebase Hosting
 // If you do not serve/host your project using Firebase Hosting see https://firebase.google.com/docs/web/setup
-importScripts('https://www.gstatic.com/firebasejs/5.0.3/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/5.0.3/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js')
+importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js')
 // Initialize Firebase
 var config = {
     'messagingSenderId': '114469939995'
